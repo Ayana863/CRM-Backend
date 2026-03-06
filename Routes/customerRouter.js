8f12e967536ddeecf290f4c96fb7e61760dbd0c9
Ayana863 <ayana863anna@gmail.com>
2026-03-06 20:24:06 +0530

    added

--- Routes/customerRouter.js
+++ Routes/customerRouter.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const validToken = require("../MIDDLEWARE/authMiddleware");
-const {
-  getCustomers,
-  addCustomer,
-  deleteCustomer,
-  updateCustomer
-} = require("../controller/customerController");
+const express = require("express")
+const validToken = require("../MIDDLEWARE/authMiddleware")
+const { getCustomers, addCustomer, deleteCustomer,updateCustomer} = require("../controller/customerController")
 
-const customerRouter = express.Router();
+const customerRouter = express.Router()
 
-// RESTful routes
-customerRouter.get("/", validToken, getCustomers);         // GET all
-customerRouter.post("/", validToken, addCustomer);         // POST new
-customerRouter.delete("/:id", validToken, deleteCustomer); // DELETE by ID
-customerRouter.put("/:id", validToken, updateCustomer);    // UPDATE by ID
 
-module.exports = customerRouter;
+customerRouter.get("/", validToken, getCustomers)      
+customerRouter.post("/", validToken, addCustomer)     
+customerRouter.delete("/:id", validToken, deleteCustomer)
+customerRouter.put("/:id", validToken, updateCustomer)
+
+module.exports = customerRouter
